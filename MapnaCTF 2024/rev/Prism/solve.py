516373cdaba55eeb4cc0aa28053f958e8fdae2df
jjchoNC <5025221001@mhs.its.ac.id>
2024-01-22 06:08:19 +0700

    fix prism

--- MapnaCTF 2024/rev/Prism/solve.py
+++ MapnaCTF 2024/rev/Prism/solve.py
@@ -1,5 +1,13 @@
 import gdb
 
 gdb.execute("file prism")
-b = gdb.execute("b start", to_string=True)
-print(b)
+gdb.execute("start")
+baseAddr = int(gdb.execute("p/a $pc", to_string=True)[5:],16) - 0x30d0
+# print(hex(baseAddr))
+
+# Got it by following strlen function call when decompiling the elf file
+printFake = 0x31C4
+printFlag = 0x3330 
+gdb.execute(f"b *{hex(baseAddr + printFake)}")
+gdb.execute(f'c')
+gdb.execute(f'j *{hex(baseAddr + printFlag)}')

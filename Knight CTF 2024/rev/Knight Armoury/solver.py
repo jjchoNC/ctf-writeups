87cd4dfe0e961277e1731c33e1d5d55fcfc42171
jjchoNC <5025221001@mhs.its.ac.id>
2024-01-27 04:22:25 +0700

    Update Knight Armoury

--- Knight CTF 2024/rev/Knight Armoury/solver.py
+++ Knight CTF 2024/rev/Knight Armoury/solver.py
@@ -1,3 +1,4 @@
+import subprocess
 import gdb
 
 checkAddr = 0x401A86
@@ -5,6 +6,6 @@ gdb.execute("file knight_armoury")
 gdb.execute(f"""b *{checkAddr}""")
 gdb.execute("r < knight_armoury")
 enc = gdb.execute("x/s $rsi", to_string=True)
-gdb.execute("shell clear")
-print(enc)
-gdb.execute("q")
+enc = enc[11:-2] + "\n"
+subprocess.run("nc 198.58.104.183 11337", shell=True, input=enc, text=True)
+# KCTF{kN1gHT_aRm0uRy_aCC3ss_GranTed}
